22940f587c224b1af1d5acd1a002acaf2c512dc0
Pugialli <joaopugialli@gmail.com>
2022-09-14 13:54:55 -0300

    aula01

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Hello React Native</Text>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#000',
+  }
 });
