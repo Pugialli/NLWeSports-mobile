157e19a63703fad4da8ce1c8091beda4930dfe67
Pugialli <joaopugialli@gmail.com>
2022-09-14 18:36:17 -0300

    aula02

---
+++ src/utils/games.ts
@@ -0,0 +1,38 @@
+export const GAMES = [
+  {
+    id: '1',
+    name: 'Dofus',
+    ads: '5',
+    cover: require('../assets/games/game-1.png')
+  },
+  {
+    id: '2',
+    name: 'Pokemon Unite',
+    ads: '8',
+    cover: require('../assets/games/game-2.png')
+  },
+  {
+    id: '3',
+    name: 'Stardew Valley',
+    ads: '10',
+    cover: require('../assets/games/game-3.png')
+  },
+  {
+    id: '4',
+    name: 'Ghost of Tsushima',
+    ads: '2',
+    cover: require('../assets/games/game-4.png')
+  },
+  {
+    id: '5',
+    name: 'Assassins Creed: Mirage',
+    ads: '1',
+    cover: require('../assets/games/game-5.png')
+  },
+  {
+    id: '6',
+    name: 'Rockband 4',
+    ads: '15',
+    cover: require('../assets/games/game-6.png')
+  },
+];
