ddc2bcfab1e0d467b56ba1871d4c640be07ad146
Pugialli <joaopugialli@gmail.com>
2022-09-16 02:24:00 -0300

    aula03 (metade)

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -7,9 +7,11 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
-    name: string;
-    ads: string;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number;
+    }
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -21,7 +23,7 @@ export function GameCard({ data, ...rest }: Props) {
         <TouchableOpacity style={styles.container} {...rest}>
             <ImageBackground
                 style={styles.cover}
-                source={data.cover}
+                source={{ uri: data.bannerUrl }}
             >
                 <LinearGradient
                     colors={THEME.COLORS.FOOTER}
@@ -29,10 +31,10 @@ export function GameCard({ data, ...rest }: Props) {
                 >
 
                     <Text style={styles.name}>
-                        {data.name}
+                        {data.title}
                     </Text>
                     <Text style={styles.ads}>
-                        {data.ads} anúncios
+                        {data._count.ads} anúncios
                     </Text>
 
                 </LinearGradient>
---
+++ src/screens/Game/index.tsx
@@ -0,0 +1,21 @@
+import { useRoute } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { GameParams } from '../../@types/navigation';
+
+import { Background } from '../../components/Background';
+
+import { styles } from './styles';
+
+export function Game() {
+
+    const route = useRoute();
+    const game = route.params as GameParams;
+    
+    return (
+        <Background>
+            <SafeAreaView style={styles.container}>
+
+            </SafeAreaView>
+        </Background>
+    );
+}
