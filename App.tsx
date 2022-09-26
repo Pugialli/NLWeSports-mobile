4387c20b35f59aaddbf964e2a33e9b4708bd5592
Pugialli <joaopugialli@gmail.com>
2022-09-26 16:35:21 -0300

    aula 06

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { useRef, useEffect } from "react";
+
 import { StatusBar } from "react-native";
 import {
   useFonts,
@@ -7,10 +9,18 @@ import {
   Inter_900Black
 } from '@expo-google-fonts/inter'
 
+import { Subscription } from 'expo-modules-core';
+import * as Notifications from 'expo-notifications'
+
 import { Routes } from "./src/routes";
 
 import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
+
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from "./src/services/getPushNotificationToken";
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,6 +30,29 @@ export default function App() {
     Inter_900Black
   })
 
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  });
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  }, []);
 
   return (
     <Background>
